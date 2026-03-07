09e79a2a1b98b47f7b22ba9db9c0391c770151dd
anabautzer <ac.bautzer@hotmail.com>
2026-03-07 14:57:54 -0300

    exercicio 1

---
+++ prog01.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls") # limpa o terminal
+# Narrativa:
+# Solicitar primeiro número:
+n1 = float(input("Número 1: "))
+# Solicitar segundo número:
+n2 = float(input("Número 2: "))
+# Solicitar terceiro número:
+n3 = float(input("Número 3: "))
+# Calcular a média dos 3 números
+media = (n1 + n2 + n3) / 3
+# Exibir a média
+print(media)

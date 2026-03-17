563e825337779585d89ba7bd964a28bd73ac0a3f
anabautzer <ac.bautzer@hotmail.com>
2026-03-17 10:04:53 -0300

    Delete prog10.py

--- prog10.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system ("cls")
-
-# Próximo múltiplo de 5
-
-num = int(input("Número: "))
-resul = num + (5 - (num % 5))
-
-print(f"O próximo multiplo de 5 a partir de {num} é {resul}")

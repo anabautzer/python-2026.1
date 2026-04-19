4ab3d64e5c804b4027e530e3c99e778f07bcbed9
Ana Carolina Bautzer <ac.bautzer@hotmail.com>
2026-04-19 16:04:29 -0300

    Update input validation for grade entry
    Fix validation logic to correctly check for valid grades between 0 and 10.

--- aula-10/prog01.py
+++ aula-10/prog01.py
@@ -5,12 +5,9 @@ os.system("cls")
 
 numero = input("Número: ")
 
-while not numero.isnumeric():
+while not numero.isnumeric() or int(numero) >= 0 and int(numero) > 10: # maior igual ao zero ainda irá dar certo, e ainda irá dar erro com número negativo
     print("Erro! Não digitou um número")
     numero = input("Número: ")
-    if numero.isnumeric() and int(numero) < 0 and int(numero) > 10:
-        print("Erro! Digite uma nota valida")
-        numero = input("Número: ")
 else:
     numero = float(numero)
 
